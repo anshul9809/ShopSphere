a3a40c834348c9fc1d2df1114b2b83bf52e5df34
anshul9809 <kumar.anshul9809@gmail.com>
2024-06-14 15:48:37 +0530

    Project completed

--- client/src/utils/index.js
+++
@@ -1,7 +0,0 @@
-export const shortenText = (text, n)=>{
-    if(text.length > n){
-        const shortenedText = text.substring(0,n).concat("...");
-        return shortenedText;
-    }
-    return text;
-}
